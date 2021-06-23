619dc818512dd57d84b9ae9beedf4850f576392c
Hammad <hamadkhalid@yahoo.com>
2021-06-23 18:41:35 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+const socketIO = require('socket.io')
+
+const PORT = 8080
+const NEW_MESSAGE_EVENT = 'new-message-event'
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+app.use(cors())
+
+const room = 'general'
+io.on('connection', socket => {
+  socket.join(room)
+
+  socket.on(NEW_MESSAGE_EVENT, data => {
+    io.in(room).emit(NEW_MESSAGE_EVENT, data)
+  })
+
+  socket.on('disconnect', () => {
+    socket.leave(room)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`)
+})
